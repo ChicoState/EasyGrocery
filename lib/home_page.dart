a4c2b76ec6cd9a2fd13aa4966b065e2089520423
Ryan Hathaway <ryanhathaway777@gmail.com>
2019-10-21 21:42:29 -0700

    Added basic home screen with welcom text and app image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,10 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
 import 'package:circular_bottom_navigation/tab_item.dart';
 
-
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({this.onSignedOut, Key key, this.title, this.auth}) : super(key: key);
+  const MyHomePage({this.onSignedOut, Key key, this.title, this.auth})
+      : super(key: key);
   final VoidCallback onSignedOut;
   final String title;
   final BaseAuth auth;
@@ -18,10 +17,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-   _signOut(BuildContext context) async {
+  _signOut(BuildContext context) async {
     try {
       await widget.auth.signOut();
-      
+
       widget.onSignedOut();
     } catch (e) {
       print(e);
@@ -30,114 +29,148 @@ class _MyHomePageState extends State<MyHomePage> {
 
   //String uid = widget.auth.currentUser().toString();
   final GlobalKey<ScaffoldState> _drawerKey = new GlobalKey<ScaffoldState>();
-  static int _currentIndex=0;
+  static int _currentIndex = 0;
 
   //controller for circular bottom nav bar
-  CircularBottomNavigationController _navigationController = 
-  new CircularBottomNavigationController(_currentIndex);
+  CircularBottomNavigationController _navigationController =
+      new CircularBottomNavigationController(_currentIndex);
   //List of tabItems for circulatr bottom navigation bar
-  List<TabItem> tabItems =  List.of([
-    new TabItem(Icons.home, "Home", Colors.green, labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.green),),
-    new TabItem(Icons.library_books, "List", Colors.green, labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.green)),
-    new TabItem(Icons.monetization_on, "Prices", Colors.green, labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.green)),
+  List<TabItem> tabItems = List.of([
+    new TabItem(
+      Icons.home,
+      "Home",
+      Colors.green,
+      labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.green),
+    ),
+    new TabItem(Icons.library_books, "List", Colors.green,
+        labelStyle:
+            TextStyle(fontWeight: FontWeight.normal, color: Colors.green)),
+    new TabItem(Icons.monetization_on, "Prices", Colors.green,
+        labelStyle:
+            TextStyle(fontWeight: FontWeight.normal, color: Colors.green)),
   ]);
-
 
   @override
   Widget build(BuildContext context) {
     final List<Widget> _pages = [
-      PlaceHolderWidget(Colors.white),
+      HomeScreen(),
       GroceryList(auth: widget.auth),
       PlaceHolderWidget(Colors.green)
     ];
     return Scaffold(
-      //Used to open the drawer by affecting the state of the scaffold
-      key: _drawerKey,
-      appBar: AppBar(
-        //Icon button that acts as a settings to logout under
-        actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.settings, color: Colors.black,),
-          onPressed: () => _drawerKey.currentState.openEndDrawer()
-          ),],
-        //Removes automatic back button on page
-        automaticallyImplyLeading: false,
-        //centers the name of the app on the appbar
-        centerTitle: true,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        backgroundColor: Colors.white,
-        title: Text(widget.title),
-        textTheme: TextTheme(
-          title: TextStyle(color: Colors.greenAccent, fontSize: 25.0, fontFamily: 'roboto')
+        //Used to open the drawer by affecting the state of the scaffold
+        key: _drawerKey,
+        appBar: AppBar(
+          //Icon button that acts as a settings to logout under
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.black,
+                ),
+                onPressed: () => _drawerKey.currentState.openEndDrawer()),
+          ],
+          //Removes automatic back button on page
+          automaticallyImplyLeading: false,
+          //centers the name of the app on the appbar
+          centerTitle: true,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          backgroundColor: Colors.white,
+          title: Text(widget.title),
+          textTheme: TextTheme(
+              title: TextStyle(
+                  color: Colors.greenAccent,
+                  fontSize: 25.0,
+                  fontFamily: 'roboto')),
         ),
-      ),
-      //Adds a drawer on the side that will act as a settings page
-      endDrawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Settings'),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
+        //Adds a drawer on the side that will act as a settings page
+        endDrawer: Drawer(
+            child: ListView(
+                // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+              DrawerHeader(
+                child: Text('Settings'),
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                ),
               ),
-            ),
-            ListTile(
-              title: Text('Logout'),
-              onTap: () { 
+              ListTile(
+                  title: Text('Logout'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    _signOut(context);
+                  }),
+              ListTile(
+                title: Text('Close'),
+                onTap: () {
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ])),
 
-                Navigator.pop(context);
-                _signOut(context);
-                
-              }
-            ),
-            ListTile(
-              title: Text('Close'),
-              onTap: () {
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ]
-      )),
+        //body
+        body: _pages[_currentIndex],
 
-      //body
-      body: _pages[_currentIndex],
-
-      //Adds Navigation bar to the bottom of the app screen
-      bottomNavigationBar: CircularBottomNavigation(
-        tabItems,
-        controller: _navigationController,
-        selectedCallback: (int selectedPos){
-          //call set state and update index of current page
-          setState(() {
-            _navigationController.value = selectedPos;
-            _currentIndex = selectedPos;
-          });
-        },
-      )
-    );
+        //Adds Navigation bar to the bottom of the app screen
+        bottomNavigationBar: CircularBottomNavigation(
+          tabItems,
+          controller: _navigationController,
+          selectedCallback: (int selectedPos) {
+            //call set state and update index of current page
+            setState(() {
+              _navigationController.value = selectedPos;
+              _currentIndex = selectedPos;
+            });
+          },
+        ));
   }
 
-  void changeTab(int index){
+  void changeTab(int index) {
     setState(() {
       _currentIndex = index;
     });
   }
 }
 
-
-
-class PlaceHolderWidget extends StatelessWidget{
+class PlaceHolderWidget extends StatelessWidget {
   final Color color;
   PlaceHolderWidget(this.color);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       color: color,
+    );
+  }
+}
+
+//Basic homescreen with welcome text and app image
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          children: <Widget>[ 
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child:Center(
+                child: Text("Welcome to Easygrocery", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0))
+              ),
+            ),
+            Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              margin: EdgeInsets.all(10.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Image.asset('assets/appicon.png', fit: BoxFit.fill)
+            )
+          ]
+        ),
     );
   }
 }
