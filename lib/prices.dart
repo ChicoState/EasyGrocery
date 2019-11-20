791fbf561e250ffd399295e6ff4f550c0ad2f510
kyle rodgers <kyle77r@gmail.com>
2019-11-19 22:29:46 -0800

    Added card builder for current stores

--- lib/prices.dart
+++ lib/prices.dart
@@ -182,9 +182,6 @@ class CompareState extends State<Compare> {
   List<dynamic> items = [];
   //User's UID
   String uid = "";
-  //Grocery stores nearby
-  List<GroceryStores> _groceryStores = <GroceryStores> [
-  ];
 
   //Initialize the state with variables
   void initState() {
@@ -225,30 +222,26 @@ class CompareState extends State<Compare> {
         ),
       body: SingleChildScrollView(
         padding: EdgeInsets.only(top: 10),
-        child: Row (
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-           /* Column(children: <Widget>[
-              showShops("walmart.jpg", 50, 125, 115)
-            ],),
-            Column(children: <Widget>[
-              showShops("safeway.png", 200, 125, 125)
-            ],),*/
-            Column(children: <Widget>[
-            ListView.builder(
-              itemCount: _groceryStores.length,
+        child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+        SizedBox(
+          height: 220,
+          child:
+        ListView.separated(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.groceryStores.length,
+              separatorBuilder: (BuildContext context, int index) => Divider(
+                indent: 30,
+              ),
               itemBuilder: (BuildContext context, int index) {
-                return showShops(
-                  _groceryStores[index].filename, 
-                  150, 
-                  _groceryStores[index].width, 
-                  _groceryStores[index].height);
+                print("Length: " + widget.groceryStores.length.toString());
+                return showShops(index);
               },
-            ) 
-            ],)
-          ],
+            ) ), ]),
         )
-      )
     );
   }
 
@@ -257,38 +250,55 @@ class CompareState extends State<Compare> {
 * and all is needed is the filename and dimensions for the image
 *
 */
-  Card showShops(String filename, int price, int width, int height) {
-        return Card( 
-        elevation: 2,
-        child: ClipPath(
-        child: Container(
-        child: Column(children: <Widget>[
-          new Image.asset(
-          'assets/$filename',
-          width: width.toDouble(),
-          height: height.toDouble(),
-          fit: BoxFit.fill,
-          ),
-          Padding(padding: EdgeInsets.only(top: 180-height.toDouble()),),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Price: \$$price")
-            ],
-            )
-        ],),
-          height: 200,
-          width: 125,
-          decoration: BoxDecoration(
-          border: Border(
-            right: BorderSide(color: Colors.green, width: 5),
-            )),
-        ),
-      clipper: ShapeBorderClipper(shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(3))),
-        ),
-        );
+  Row showShops(int index) {
+    //Initiliaze shop variables
+        GroceryStores store = widget.groceryStores[index];
+        String filename = store.filename;
+        int width = store.width;
+        int height = store.height;
+        int price = 200;
+        print(index);
+        print(store.name);
+        print(store.selected);
+        if (store.selected) {
+          return 
+        Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Column(children: <Widget>[
+            Card( 
+            elevation: 2,
+            child: ClipPath(
+            child: Container(
+            child: Column(children: <Widget>[
+              new Image.asset(
+              'assets/$filename',
+              width: width.toDouble(),
+              height: height.toDouble(),
+              fit: BoxFit.fill,
+              ),
+              Padding(padding: EdgeInsets.only(top: 180-height.toDouble()),),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Price: \$$price")
+                ],
+                )
+            ],),
+              height: 200,
+              width: 125,
+              decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(color: Colors.green, width: 5),
+                )),
+            ),
+          clipper: ShapeBorderClipper(shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(3))),
+            ),
+          )])]);}
+          else {return null;}
   }
 }
