1efb6ff28d21acea2c6e17e9f9b355918144f119
Ryan Hathaway <ryanhathaway777@gmail.com>
2019-12-13 19:31:02 -0800

    Add new tests file

---
+++ test/widget_tests/tests.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:EasyGrocery/auth.dart';
+import 'package:EasyGrocery/home_page.dart';
+import 'package:EasyGrocery/list.dart';
+import 'package:EasyGrocery/login.dart';
+import 'package:EasyGrocery/main.dart';
+import 'package:EasyGrocery/prices.dart';
+import 'package:EasyGrocery/register.dart';
+import 'package:EasyGrocery/search.dart';
+import 'package:EasyGrocery/shoplist.dart';
+import 'package:EasyGrocery/splash.dart';
+
+
+void main(){
+  //calling test widget
+ testWidgets('validate appBar string', (WidgetTester tester) async {
+   //set up what to test static on the widget
+   // creates a temporary widget to find
+   await tester.pumpWidget(MaterialApp(
+       home: Scaffold(
+         //appbar text
+         appBar: AppBar(
+           title: Text('EasyGrocery'),
+         ),
+       )
+   ));
+   //
+   expect(find.text('EasyGrocery'), findsOneWidget);
+ });
+
+ testWidgets('Check appbar head', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
+    expect(find.widgetWithText(AppBar, 'EasyGrocery'), findsOneWidget);
+  });
+}
